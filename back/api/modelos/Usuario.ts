f87c555877da1cb174e3174ab3375e104f21fd58
crisgon123 <crisgon123@gmail.com>
2019-11-09 11:47:21 -0500

    prueba

--- back/api/modelos/Usuario.ts
+++ back/api/modelos/Usuario.ts
@@ -1,9 +1,11 @@
 import { DataTypes, Sequelize } from 'sequelize';
-import { Usuario } from '../configuracion/sequelize';
+
 const crypto = require('crypto');
 var jwt = require('jsonwebtoken')
-export let usuario_model = (sequelize: Sequelize) => {
-    var usuario = sequelize.define('usuario', {
+
+export let destino_model = (sequelize: Sequelize) => {
+
+  let usuario = sequelize.define('t_usuario', {
         usu_id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
@@ -26,26 +28,26 @@ export let usuario_model = (sequelize: Sequelize) => {
         }
         
     },{
-        tableName:'usuario',
+        tableName:'t_usuario',
         timestamps: false
     });
     // Sirve para encriptar la contraseña ingresada por el usuario
-    usuario.prototype.setSaltYHash = function (password:any) {
-        this.usu_salt = crypto.randomBytes(16).toString('hex');
-        this.usu_hash = crypto.pbkdf2Sync(password,this.usu_salt,1000,64,'sha512').toString('hex');
-    }
-    usuario.prototype.validPass =function(password:any){
-        let usu_hash_temp = crypto.pbkdf2Sync(password,this.usu_salt,1000,64, 'sha512').toString('hex')
-        return usu_hash_temp === this.usu_hash;
-    }
-    usuario.prototype.generarJWT = function(){
-        let payload ={
-            usu_id: this.usu_id,
-            usu_nom: '${this.usu_nom}' ${this.usu_ape}
-        }
-        let token = jwt.sign(payload, 'codigo6',{expiresIn:'1h'},
-        {algoritm:'R5256'})
-        return token;
-    }
+    // usuario.prototype.setSaltYHash = function (password:any) {
+    //     this.usu_salt = crypto.randomBytes(16).toString('hex');
+    //     this.usu_hash = crypto.pbkdf2Sync(password,this.usu_salt,1000,64,'sha512').toString('hex');
+    // }
+    // usuario.prototype.validPass =function(password:any){
+    //     let usu_hash_temp = crypto.pbkdf2Sync(password,this.usu_salt,1000,64, 'sha512').toString('hex')
+    //     return usu_hash_temp === this.usu_hash;
+    // }
+    // usuario.prototype.generarJWT = function(){
+    //     let payload ={
+    //         usu_id: this.usu_id,
+    //         usu_nom: '${this.usu_nom}' ${this.usu_ape}
+    //     }
+    //     let token = jwt.sign(payload, 'codigo6',{expiresIn:'1h'},
+    //     {algoritm:'R5256'})
+    //     return token;
+    // }
     return usuario;
 }
