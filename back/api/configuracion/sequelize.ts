43adf53319699b592572046b95ad032101ab4049
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2019-11-09 12:31:09 -0500

    actualizacion final de MODULOS

--- back/api/configuracion/sequelize.ts
+++ back/api/configuracion/sequelize.ts
@@ -7,6 +7,8 @@ import { ordendetalle_model } from "../modelos/Ordendetalle";
 import { persona_model } from "../modelos/Persona";
 import { producto_model } from "../modelos/Producto";
 import { usuario_model } from "../modelos/Usuario";
+import { metodopago_model } from "../modelos/MetodoPago";
+import { orden_model } from "../modelos/Orden";
 
 const Sequelize = require("sequelize");
 
@@ -32,3 +34,5 @@ export const Ordendetalle: any = ordendetalle_model(conexion);
 export const Persona: any = persona_model(conexion);
 export const Producto: any = producto_model(conexion);
 export const Usuario: any = usuario_model(conexion);
+export const MetodoPago: any = metodopago_model(conexion);
+export const Orden: any = orden_model(conexion);
