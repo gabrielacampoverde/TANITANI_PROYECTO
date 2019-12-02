7a61b02417544cdf861eef7afc0e42bce25c0561
Jacky_Paola <jackelineyacky@gmail.com>
2019-12-01 22:35:42 -0500

    add footer

--- react/proyecto/src/App.js
+++ react/proyecto/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './styles/App.sass';
 import SimpleSlider from './Component/SliderView/slider'
 import Header from './Component/Header/Header'
+import Footer from './Component/Footer/Footer'
 export default class componentName extends Component{
   render(){
      return(
       <Fragment>
         <Header></Header>
         <SimpleSlider></SimpleSlider>
+        <Footer/>
       </Fragment>
   
     )
--- react/proyecto/src/Component/Footer/Footer.js
+++ react/proyecto/src/Component/Footer/Footer.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import {baseUrl} from "./../../config";
 
 export default class Footer extends Component {
-    render() {
-        return ( <
-            div > textInComponent < /div>
-        );
-    }
+  render() {
+    return (
+      <footer>
+          <div className="cont-footer">
+            <div className="flex-footer">
+                <div className="item-footer">
+                    <a href="">
+                        <img src={baseUrl + "/logo.png"} />
+                    </a>
+                </div>
+                <div className="item-footer">
+                    <ul>
+                        <li><a href="">CONTÁCTANOS</a></li>
+                        <li><a href="">FORMAS DE PAGO</a></li>
+                        <li><a href="">ORDEN DE PEDIDO</a></li>
+                    </ul>
+                </div>
+                <div className="item-footer">
+                    <h5>10% DE DESCUENTO EN TU PRIMER PEDIDO</h5>
+                    <input type="email" name="email" id="email" placeholder="Correo Electrtónico"/>
+                    <ul>
+                        <li><a href=""><i className="icon-whatsapp"></i></a></li>
+                        <li><a href=""><i className="icon-social-facebook"></i></a></li>
+                        <li><a href=""><i className="icon-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="copy">
+                <p>&copy; 2019 Tanitani | Todos los Derechos Reservados</p>
+            </div>
+        </div>
+      </footer>
+    );
+  }
 }
