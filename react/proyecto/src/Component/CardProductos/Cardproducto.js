933e7aae336ae29acc9313cf4d55adef69ae5a93
crisgon123 <crisgon123@gmail.com>
2019-12-04 21:44:15 -0500

    Card

--- react/proyecto/src/Component/CardProductos/Cardproducto.js
+++ react/proyecto/src/Component/CardProductos/Cardproducto.js
@@ -13,7 +13,31 @@ export default class Cardproducto extends Component {
                         <div className="card">
                             <div>
                                 <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="text">
+                                <div className="textproduct">
+                                    <h4>Bouquete Rosas Forever</h4>
+                                    <h5>s/. 75</h5>
+                                </div>
+                            </div>
+                            <div className="hover">
+                                <div className="cont-hover">
+                                    <div className="flex-content">
+                                        <div className="item">
+                                            <i className="icon-heart"></i>
+                                        </div>
+                                        <div className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </div>
+                                        <div className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                           
+                        </div>
+                        <div className="card">
+                            <div>
+                                <img src={baseUrl + "/bouquet-1.png"} /> 
+                                <div className="textproduct">
                                     <h4>Bouquete Rosas Forever</h4>
                                     <h5>s/. 75</h5>
                                 </div>
@@ -23,24 +47,12 @@ export default class Cardproducto extends Component {
                         <div className="card">
                             <div>
                                 <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="text">
+                                <div className="textproduct">
                                     <h4>Bouquete Rosas Forever</h4>
                                     <h5>s/. 75</h5>
                                 </div>
                             </div>
-                     
                         </div>
-                        <div className="card">
-                            <div>
-                                <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="text">
-                                    <h4>Bouquete Rosas Forever</h4>
-                                    <h5>s/. 75</h5>
-                                </div>
-                            </div>
-                     
-                        </div>
-                        
                     </div>
                 </div>
             </div>
