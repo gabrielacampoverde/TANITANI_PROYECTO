dbfb989bcd4a09f1cab881c5af5b57a5b36cd19a
RenzoGCh <r.gonxalezch@gmail.com>
2019-12-07 19:14:43 -0500

    Eliminando

--- proyecto_Tanitani/src/app/app.module.ts
+++ proyecto_Tanitani/src/app/app.module.ts
@@ -4,33 +4,21 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './component/home/home.component';
-import { FormularioComponent } from './component/formulario/formulario.component';
+
 
 import {RUTAS} from './app.routes';
-import { CrearDatosComponent } from './component/crear-datos/crear-datos.component';
+
 import {FormsModule} from '@angular/forms';
 import { FilterPipe } from './pipes/filter.pipe';
 
 
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-
-import { ComplementosComponent } from './component/complementos/complementos.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FormularioComponent,
-    CrearDatosComponent,
-    FilterPipe,
-    HeaderComponent,
-    FooterComponent,
-    ComplementosComponent
-
+    FilterPipe
   ],
   imports: [
     BrowserModule,
--- proyecto_Tanitani/src/app/app.routes.ts
+++ proyecto_Tanitani/src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './component/home/home.component';
-import {FormularioComponent} from './component/formulario/formulario.component';
-import { CrearDatosComponent } from './component/crear-datos/crear-datos.component';
 
-import { ComplementosComponent } from './component/complementos/complementos.component';
 
 const caminos: Routes = [
-    {path: ' ', component: HomeComponent},
-    {path: 'home', component: HomeComponent},
-    {path: 'formulario', component: FormularioComponent},
-    {path: 'formulario/crearDatos', component: CrearDatosComponent},
-    //{path: 'promociones', component: PromocionesComponent},
-    {path: 'complementos', component: ComplementosComponent}
+  
 ];
 export const RUTAS = RouterModule.forRoot(caminos);
