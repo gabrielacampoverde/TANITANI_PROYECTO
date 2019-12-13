95612faa29b51ba00293b66fc4a78147f0d0c63b
gabrielacampoverde <gabriela.campoverde@gmail.com>
2019-12-12 20:30:00 -0500

    add change

--- react/proyecto/src/Component/Promociones/Promociones.js
+++ react/proyecto/src/Component/Promociones/Promociones.js
@@ -1,44 +1,57 @@
 import React, { Component } from 'react';
 
 export default class Promociones extends Component {
-   productCard = (e) =>{
-   //    $(this).addClass('animate');
-   //     $('div.carouselNext, div.carouselPrev').addClass('visible');			
-   //   }, function(){
-   //     $(this).removeClass('animate');			
-   //     $('div.carouselNext, div.carouselPrev').removeClass('visible');
-  }
-  
-//   https://codepen.io/virgilpana/pen/RNYQwB/
    render() {
       return (
-         <div>
-            <h1>PROMOCIONES</h1>
-
-            <div id="make-3D-space">
-               <div onChange={this.productCard}>
-                  <div id="product-front">
-                     <div class="shadow"></div>
-                     <img src="https://tinysurprise.com/media/catalog/product/cache/1/image/330x430/9df78eab33525d08d6e5fb8d27136e95/r/e/redrose_mixed_300x300.png" alt="" />
-                     <div class="image_overlay"></div>
-                     <div id="view_details">View details</div>
-                     <div class="stats">
-                        <div class="stats-container">
-                           <span class="product_price">$50</span>
-                           <span class="product_name">Bouquet de rosas "FOREVER"</span>
-
-                           <div class="product-options">
-                              <div class="colors">
-                                 <div class="c-red"><span>Agregar al Carro</span></div>
+         <div className="sectionProductos">
+            <div className="container">
+               <div className="flex">
+                  <div className="card">
+                     <div>
+                        <img src={baseUrl + "/bouquet-1.png"} />
+                        <div className="textproduct">
+                           <h4>Bouquete Rosas Forever</h4>
+                           <h5>s/. 75</h5>
+                        </div>
+                     </div>
+                     <div className="hover">
+                        <div className="cont-hover">
+                           <div className="flex-content">
+                              <div className="item">
+                                 <i className="icon-heart"></i>
+                              </div>
+                              <div className="item">
+                                 <i className="icon-magnifier"></i>
+                              </div>
+                              <div className="item">
+                                 <i className="icon-shopping-cart"></i>
                               </div>
                            </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div className="card">
+                     <div>
+                        <img src={baseUrl + "/bouquet-1.png"} />
+                        <div className="textproduct">
+                           <h4>Bouquete Rosas Forever</h4>
+                           <h5>s/. 75</h5>
+                        </div>
+                     </div>
+
+                  </div>
+                  <div className="card">
+                     <div>
+                        <img src={baseUrl + "/bouquet-1.png"} />
+                        <div className="textproduct">
+                           <h4>Bouquete Rosas Forever</h4>
+                           <h5>s/. 75</h5>
                         </div>
                      </div>
                   </div>
                </div>
             </div>
          </div>
-            );
-          }
-        }
-      
+      )
+   }
+}
