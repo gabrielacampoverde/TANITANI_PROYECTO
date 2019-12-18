dba0d109cea8963c336c6350123bf4ab1f8aeb52
Dianitagv <dgallegosmkt@gmail.com>
2019-12-17 22:43:25 -0500

    card-prodcuitos

--- react/proyecto/src/Component/CardProductos/Cardproducto.js
+++ react/proyecto/src/Component/CardProductos/Cardproducto.js
@@ -9,23 +9,20 @@ export default class Cardproducto extends Component {
         return (
             <div className="sectionProductos">
                 <div className="banner-red">
-                    <p>
                     <h4>“El amor espera en el borde de un pétalo.”</h4>
-                    </p>
                 </div>
                 <div className="container">
-                    <div className="flex">
-                        <div className="card">
-                            <div>
+                    <div className="product-list">
+                        <div className="product-item card">
+                            <div className="product-image">
                                 <img src={baseUrl + "/bouquet-1.png"} /> 
                                 <div className="textproduct">
                                     <h4>Bouquete Rosas Forever</h4>
                                     <h5>s/. 75</h5>
                                 </div>
                             </div>
-                            <div className="hover">
-                                <div className="cont-hover">
-                                    <div className="flex-content">
+                            <div className="buttons-actions flex">
+                                <div className="flex-content">
                                         <div className="item">
                                             <i className="icon-heart"></i>
                                         </div>
@@ -35,7 +32,9 @@ export default class Cardproducto extends Component {
                                         <div className="item">
                                             <i className="icon-shopping-cart"></i>
                                         </div>
-                                    </div>
+                                </div>
+                                <div className="btn-add-to-cart">
+                                    <button className="btn btn-success btn-block">Agregar al carrito</button>
                                 </div>
                             </div>                           
                         </div>
