b8a72c19603a5d9db95ae26434f07c38e726089d
Dianitagv <dgallegosmkt@gmail.com>
2019-12-18 00:59:46 -0500

    Landing, card producto, hover, footer, banner

--- react/proyecto/src/Component/CardProductos/Cardproducto.js
+++ react/proyecto/src/Component/CardProductos/Cardproducto.js
@@ -7,58 +7,174 @@ export default class Cardproducto extends Component {
     render() {
 
         return (
-            <div className="sectionProductos">
-                <div className="banner-red">
+            <div>
+                <div className="banner-product">
                     <h4>“El amor espera en el borde de un pétalo.”</h4>
                 </div>
+            <div className="sectionProductos">
+                
                 <div className="container">
                     <div className="product-list">
                         <div className="product-item card">
                             <div className="product-image">
-                                <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="textproduct">
-                                    <h4>Bouquete Rosas Forever</h4>
-                                    <h5>s/. 75</h5>
-                                </div>
-                            </div>
-                            <div className="buttons-actions flex">
-                                <div className="flex-content">
-                                        <div className="item">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
                                             <i className="icon-heart"></i>
-                                        </div>
-                                        <div className="item">
+                                        </a>
+                                        <a href="#"  className="item">
                                             <i className="icon-magnifier"></i>
-                                        </div>
-                                        <div className="item">
+                                        </a>
+                                        <a href="#"  className="item">
                                             <i className="icon-shopping-cart"></i>
-                                        </div>
-                                </div>
-                                <div className="btn-add-to-cart">
-                                    <button className="btn btn-success btn-block">Agregar al carrito</button>
-                                </div>
-                            </div>                           
-                        </div>
-                        <div className="card">
-                            <div>
-                                <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="textproduct">
-                                    <h4>Bouquete Rosas Forever</h4>
-                                    <h5>s/. 75</h5>
-                                </div>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
                             </div>
-                     
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
                         </div>
-                        <div className="card">
-                            <div>
-                                <img src={baseUrl + "/bouquet-1.png"} /> 
-                                <div className="textproduct">
-                                    <h4>Bouquete Rosas Forever</h4>
-                                    <h5>s/. 75</h5>
-                                </div>
+                        <div className="product-item card">
+                            <div className="product-image">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
+                                            <i className="icon-heart"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
                             </div>
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
+                        </div>
+                        <div className="product-item card">
+                            <div className="product-image">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
+                                            <i className="icon-heart"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
+                            </div>
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
+                        </div>
+                        <div className="product-item card">
+                            <div className="product-image">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
+                                            <i className="icon-heart"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
+                            </div>
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
+                        </div>
+                        <div className="product-item card">
+                            <div className="product-image">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
+                                            <i className="icon-heart"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
+                            </div>
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
+                        </div>
+                        <div className="product-item card">
+                            <div className="product-image">
+                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
+                                <div className="buttons-actions">
+                                    <div className="icons">
+                                        <a href="#" className="item">
+                                            <i className="icon-heart"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-magnifier"></i>
+                                        </a>
+                                        <a href="#"  className="item">
+                                            <i className="icon-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                    <div className="btn-add-to-cart">
+                                        <button className="btn-add">Agregar al carrito</button>
+                                    </div>
+                                </div> 
+                            </div>
+                             
+                            <div className="product-text">
+                                <h4>Bouquete Rosas Forever</h4>
+                                <h5>S/ 75.00</h5>
+                            </div>                         
                         </div>
                     </div>
                 </div>
+            </div>
+            
             </div>
         )
     }
