5540d94a9e014db54abf6286e0edf2a49a72a3c2
Dianitagv <dgallegosmkt@gmail.com>
2019-12-27 22:39:35 -0500

    responsive mao

--- react/proyecto/src/Component/CardProductos/Cardproducto.js
+++ react/proyecto/src/Component/CardProductos/Cardproducto.js
@@ -11,32 +11,33 @@ export default class Cardproducto extends Component {
                 <div className="banner-product">
                     <h4>“El amor espera en el borde de un pétalo.”</h4>
                 </div>
-            <div className="sectionProductos">
-                <div className="container">
-                    <div className="titlecard">
-                        <h1>DESTACADOS</h1>
-                    </div>
-                    <div className="product-list">
-                        <div className="product-item card">
-                            <div className="product-image">
-                                <img src={baseUrl + "/otras_img/bouquet-2.png"} /> 
-                                <div className="buttons-actions">
-                                    <div className="icons">
-                                        <a href="#" className="item">
-                                            <i className="icon-heart"></i>
-                                        </a>
-                                        <a href="#"  className="item">
-                                            <i className="icon-magnifier"></i>
-                                        </a>
-                                        <a href="#"  className="item">
-                                            <i className="icon-shopping-cart"></i>
-                                        </a>
+                <div className="sectionProductos">
+                    <div className="container">
+                        <div className="titlecard">
+                            <h1>DESTACADOS</h1>
+                        </div>
+                        <div className="product-list">
+                            <div className="product-item card">
+                                <div className="product-image">
+                                    <img src={baseUrl + "/otras_img/bouquet-2.png"} />
+                                    <div className="buttons-actions">
+                                        <div className="icons">
+                                            <a href="#" className="item">
+                                                <i className="icon-heart"></i>
+                                            </a>
+                                            <a href="#" className="item">
+                                                <i className="icon-magnifier"></i>
+                                            </a>
+                                            <a href="#" className="item">
+                                                <i className="icon-shopping-cart"></i>
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div className="product-text">
-                                    <h4>Bouquete Rosas Forever</h4>
-                                    <h5>S/ 75.00</h5>
+                                    <div className="product-text">
+                                        <h4>Bouquete Rosas Forever</h4>
+                                        <h5>S/ 75.00</h5>
+                                    </div>
                                 </div>
                             </div>
                             <div className="product-item card">
@@ -172,7 +173,6 @@ export default class Cardproducto extends Component {
                         </div>
                     </div>
                 </div>
-</div>
             </div>
         )
     }
