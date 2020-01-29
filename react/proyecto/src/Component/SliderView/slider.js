2eff322c0f845f707d3fd24e841b8f03920b9dfc
Dianitagv <dgallegosmkt@gmail.com>
2020-01-29 00:07:45 -0500

    tipo de letra, background, y animaciones

--- react/proyecto/src/Component/SliderView/slider.js
+++ react/proyecto/src/Component/SliderView/slider.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import {baseUrl} from "./../../config";
+import {Animated} from "react-animated-css";
 
 // const images={
 //     {src: baseURL + "/abstract01.jpg"};
@@ -20,21 +21,31 @@ export default class Banner extends Component {
       <div className="slider-home">
         <Slider {...settings}>
           <div className="item">
+         
             <img src={baseUrl + "/floresyyo.jpg"} />
+           
             <div className="caption center">
-              <h3>El amor <br/> espera en el borde <br/> de un pétalo</h3>
+            <Animated animationIn="fadeInLeft" animationOut="flash" animationInDuration={1800} animationOutDuration={1800} isVisible={true}>
+              <h3>El amor <br/> no hay que comprenderlo <br/> sino sentirlo</h3>
+             </Animated>
             </div>
           </div>
           <div className="item">
             <img src={baseUrl + "/bouquet-of-flowers.jpg"} />
             <div className="caption left">
+            <Animated animationIn="fadeInLeft" animationOut="flash" animationInDuration={1800} animationOutDuration={1800} isVisible={true}>
               <h3>El amor <br/> espera en el borde <br/> de un pétalo</h3>
+              </Animated>
             </div>
           </div>
           <div className="item">
             <img src={baseUrl + "/tulipanes.jpg"} />
             <div className="caption right">
-              <h3>El amor <br/> espera en el borde <br/> de un pétalo</h3>
+            <Animated animationIn="fadeInRight" animationOut="flash" animationInDuration={1800} animationOutDuration={1800} isVisible={true}>
+
+              <h3>Una Rosa<br/> para un persona <br/> super especial </h3>
+              </Animated>
+              
             </div>
           </div>
         </Slider>
